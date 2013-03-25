1469831d21a0a4751de07ee6042da914d8c45479
genejones <generjones@gmail.com>
2013-03-25 01:31:57 -0400

    Changed example text on send.js to reflect querystring parsing, multiple variables

--- send.js
+++ send.js
@@ -1,4 +1,4 @@
 var req = new XMLHttpRequest();
 req.open( 'PUT', 'http://127.0.0.1:1337/', true);
-req.send('something here');
+req.send('something%20here=true&yay=true&hello=world');
 console.log("We sent something!");
