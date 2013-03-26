f3e863cb3dc4de4803699f1c6ec4c16fed118555
genejones <generjones@gmail.com>
2013-03-25 21:24:01 -0400

    Added document.images method of Omni beacon detection

--- send.js
+++ send.js
@@ -1,4 +1,34 @@
-var req = new XMLHttpRequest();
-req.open( 'PUT', 'http://127.0.0.1:1337/', true);
-req.send('something%20here=true&yay=true&hello=world');
-console.log("We sent something!");
+//requires https://raw.github.com/LiosK/UUID.js/master/src/uuid.core.js to be placed before it
+//only checks for omniture beacons on first run...
+var collector = window._hddata;
+pageHash = UUID.generate(); //a unique pageHash to corelate omniture results with collector items.
+
+function check_for_beacons(){
+    if (window.opener.document.images) {
+	    for (var image_num = 0;image_num < window.opener.document.images.length;image_num++) {
+		    var src = window.opener.document.images[image_num].src;
+		    if (src.indexOf('/b/ss/') >= 0) {
+                //found an omniture beacon on the page
+                make_request(src);
+		    }
+	    }
+    }
+}
+
+function make_request(omniture_src){
+    var req = new XMLHttpRequest();
+    req.open( 'PUT', 'http://127.0.0.1:1337/', true);
+    prelim = JSON.parse( JSON.stringify(collector) );
+    prelim['pageID'] = pageHash;
+    prelim['omniture_src'] = omniture_src;
+    req.send( JSON.stringify(prelim) );
+    console.log("We sent something with a hash of " + pageHash);
+}
+
+check_for_beacons(); //check for beacons
+
+//options for seeing beacons earlier
+//http://stackoverflow.com/questions/4780822/how-can-i-detect-when-a-new-element-has-been-added-to-the-document-in-jquery
+//http://docs.jquery.com/Plugins/livequery
+//http://sinonjs.org/docs/#spies
+
